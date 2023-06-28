11d404b45b594d77debc3a924319dd5d0da645db
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2023-06-27 21:36:06 -0400

    Update main.c

--- source/main.c
+++ source/main.c
@@ -11,11 +11,11 @@ int main(int argc, char **argv)
 	FILE *defaultcfg = fopen("fs:/vol/external01/wiiu/environments/default.cfg","w");
 		fputs("aroma", defaultcfg);
 		fclose(defaultcfg);
-	FILE *autobootcfg = fopen("fs:/vol/external01/wiiu/environments/aromam/autoboot.cfg", "w");
+	FILE *autobootcfg = fopen("fs:/vol/external01/wiiu/environments/aroma/autoboot.cfg", "w");
 		fputs("wiiu_menu", autobootcfg);
-		fclose(arfautobootcfg);
+		fclose(autobootcfg);
 
-	OSLaunchTitlel(OS_TITLE_ID_REBOOT, 0);
+	OSLaunchTitle(OS_TITLE_ID_REBOOT, 0);
 	while (WHBProcIsRunning()) {}
 	WHBProcShutdown();
 	return 0;
