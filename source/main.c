28747c90a8e730720d7aa5472ca23bd408a48f39
Trace <atw3710@gmail.com>
2023-06-27 23:15:10 -0400

    Fixes

--- source/main.c
+++ source/main.c
@@ -4,27 +4,27 @@ int main(int argc, char **argv)
 {
         bool isAroma = true;
         char* environment = GetEnvironmentName();
-        if (strcmp(environment, "tiramisu") == 0)
+        if (strcmp(environment, "tiramisu") == 0) // if environment is tiramisu, you arent on aroma xD
               isAroma = false;
 
 	FILE *defaultcfg = fopen("fs:/vol/external01/wiiu/environments/default.cfg","w");
-        if (isAroma)
-          fputs("aroma", defaultcfg);
-        else
+        if (isAroma) // if on aroma set default to tiramisu
           fputs("tiramisu", defaultcfg);
+        else // if on tiramisu set default to aroma
+          fputs("aroma", defaultcfg);
 
         fclose(defaultcfg);
 
         FILE *autobootcfg;
-        if (isAroma)
+        if (isAroma) // if on aroma set tiramisu to homebrew_launcher
         {
-	  autobootcfg = fopen("fs:/vol/external01/wiiu/environments/aroma/autoboot.cfg", "w");
-          fputs("wiiu_menu", autobootcfg);
+	  autobootcfg = fopen("fs:/vol/external01/wiiu/environments/tiramisu/autoboot.cfg", "w");
+          fputs("homebrew_launcher", autobootcfg);
         }
-        else
+        else // if on tiramisu set aroma to wiiu_menu
         {
-          autobootcfg = fopen("fs:/vol/external01/wiiu/environments/tiramisu/autoboot.cfg", "w");
-          fputs("homebrew_launcher", defaultcfg);
+          autobootcfg = fopen("fs:/vol/external01/wiiu/environments/aroma/autoboot.cfg", "w");
+          fputs("wiiu_menu", defaultcfg);
         }
 
         fclose(autobootcfg);
